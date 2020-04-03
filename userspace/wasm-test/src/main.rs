8d1035c9310d51c6b401dae51ddd066cf9543b12
nielsdos <7771979+nielsdos@users.noreply.github.com>
2020-04-03 18:00:33 +0200

    More complex test

--- userspace/wasm-test/src/main.rs
+++ userspace/wasm-test/src/main.rs
@@ -1,3 +1,15 @@
+fn other_function(n: i32) -> (i32, i32) {
+    let mut vec = Vec::new();
+    for i in 0..n {
+        vec.push(i);
+    }
+
+    (vec.iter().sum::<i32>(), (n - 1) * n / 2)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n = 10000;
+    println!("Hello, world! This is a Rust program compiled with the wasm32-wasi toolchain.");
+    println!("The .wasm file (this program) was put in an initrd and this is now executing.");
+    println!("{}: {:?}", n, other_function(n));
 }
